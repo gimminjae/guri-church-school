13c89108d1abfcdafd410f6cd705a0a57f8a2e4f
mj <min356812@idrsystems.co.kr>
2025-05-14 18:57:42 +0900

    화면 아래 공간 확보

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 pt-16">
+      <main className="flex-1 pt-16 mb-30">
         {children}
       </main>
     </div>
